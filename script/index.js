6ff1bd65d9a0e81fc023dfe08de5730acfae4288
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-04 15:28:44 +0100

    aggiunta calcolo prezzo biglietto in base ai km

---
+++ script/index.js
@@ -0,0 +1,7 @@
+const kmDaPercorrere = parseInt(prompt("Quanti Km deve percorrere?"));
+
+const prezzoAlkm = (0.21);
+
+let prezzoBiglietto = (kmDaPercorrere * prezzoAlkm + " €");
+
+console.log(prezzoBiglietto);
