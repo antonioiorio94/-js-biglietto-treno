d39ad744fb15b1b156435d7610d83ae5dbaf7647
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-04 16:19:07 +0100

    aggiunta condizioni

--- script/index.js
+++ script/index.js
@@ -1,7 +1,23 @@
-const kmDaPercorrere = parseInt(prompt("Quanti Km deve percorrere?"));
+const kmDaPercorrere = parseInt(prompt("Ciao, quanti Km deve percorrere?"));
+
+const età = parseInt(prompt("Quanti anni hai?"));
 
 const prezzoAlkm = (0.21);
 
-let prezzoBiglietto = (kmDaPercorrere * prezzoAlkm + " €");
+let prezzoBiglietto = parseFloat(kmDaPercorrere * prezzoAlkm);
 
-console.log(prezzoBiglietto);
+
+
+const scontoUnder18 = (prezzoBiglietto * 20 / 100);
+const scontoOver65 = (prezzoBiglietto * 40 / 100);
+
+if (età <= 18) {
+    let prezzoUnder18 = ((prezzoBiglietto - scontoUnder18).toFixed(2) + " €");
+    console.log(prezzoUnder18);
+} else if (età >= 65) {
+    let prezzoOver65 = ((prezzoBiglietto - scontoOver65).toFixed(2) + " €");
+    console.log(prezzoOver65);
+}
+else {
+    console.log(prezzoBiglietto + " €");
+}
