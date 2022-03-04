38a1d604c069fc2fdaf730b92f1795c2cb715841
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-04 16:47:39 +0100

    aggiunta css con bootstrap

--- script/index.js
+++ script/index.js
@@ -13,11 +13,11 @@ const scontoOver65 = (prezzoBiglietto * 40 / 100);
 
 if (età <= 18) {
     let prezzoUnder18 = ((prezzoBiglietto - scontoUnder18).toFixed(2) + " €");
-    console.log(prezzoUnder18);
+    document.getElementById("prezzo18").innerText = prezzoUnder18;
 } else if (età >= 65) {
     let prezzoOver65 = ((prezzoBiglietto - scontoOver65).toFixed(2) + " €");
-    console.log(prezzoOver65);
+    document.getElementById("prezzo65").innerText = prezzoOver65;
 }
 else {
-    console.log(prezzoBiglietto + " €");
+    document.getElementById("prezzopieno").innerText = (prezzoBiglietto + " €");
 }
